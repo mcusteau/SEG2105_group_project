34d3ee4f0f860b02e5fcf6c04e57010875443043
Michel Custeau <custeaumichel@gmail.com>
2021-10-07 21:22:05 -0400

    created Login page

---
+++ app/src/main/java/com/example/seg2105_group_project/Login.java
@@ -0,0 +1,20 @@
+package com.example.seg2105_group_project;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Login {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
